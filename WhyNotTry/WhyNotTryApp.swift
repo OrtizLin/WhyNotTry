73ab5b8b3a963f3fe3ba0356cd1f27319a0137dd
Otis's Macbook Pro <fmradioo123@yahoo.com>
2024-07-27 01:33:12 +0800

    init commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Otis on 2024/7/27.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
